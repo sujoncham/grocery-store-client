79c443ba03f0cf2fedfd5bdb97ca77f234fe1624
sujon-chambugong <sujonchambugong@gmail.com>
2022-04-29 00:28:55 +0600

    react rounter added

--- src/App.js
+++ src/App.js
@@ -1,21 +1,27 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { Route, Routes } from 'react-router-dom';
 import './App.css';
-import Header from './components/SharedPart/Header';
+import AddStock from './components/AddStock/AddStock';
+import About from './components/Pages/About';
+import Blog from './components/Pages/Blog';
+import Home from './components/Pages/Home';
+import Products from './components/Pages/Inventory';
+import Page404 from './components/Pages/Page404';
 import Footer from './components/SharedPart/Footer';
+import Header from './components/SharedPart/Header';
 
 function App() {
   return (
     <div className="App">
       <Header></Header>
       <Routes>
+        <Route path='/' element={<Home></Home>}></Route>
+        <Route path='/about' element={<About></About>}></Route>
+        <Route path='/inventory' element={<Products></Products>}></Route>
+        <Route path='/blog' element={<Blog></Blog>}></Route>
+        <Route path='/addStock' element={<AddStock></AddStock>}></Route>
         <Route path='/'></Route>
-        <Route path='/'></Route>
-        <Route path='/'></Route>
-        <Route path='/'></Route>
-        <Route path='/'></Route>
-        <Route path='/'></Route>
-        <Route path='/'></Route>
+        <Route path='*' element={<Page404></Page404>}></Route>
       </Routes>
       <Footer></Footer>
     </div>
--- src/components/SharedPart/Header.js
+++ src/components/SharedPart/Header.js
@@ -11,12 +11,12 @@ const Header = () => {
                 <Navbar.Collapse id="responsive-navbar-nav">
                 
                 <Nav className="ms-auto">
-                    <Nav.Link as={Link} to="/">Home</Nav.Link>
-                    <Nav.Link as={Link} to="/">About</Nav.Link>
-                    <Nav.Link as={Link} to="/">Inventory</Nav.Link>
-                    <Nav.Link as={Link} to="/">Blog</Nav.Link>
-                    <Nav.Link as={Link} to="/">Login</Nav.Link>
-                    <Nav.Link as={Link} to="/">AddStock</Nav.Link>
+                    <Nav.Link as={Link} to="/home">Home</Nav.Link>
+                    <Nav.Link as={Link} to="/about">About</Nav.Link>
+                    <Nav.Link as={Link} to="/inventory">Inventory</Nav.Link>
+                    <Nav.Link as={Link} to="/blog">Blog</Nav.Link>
+                    <Nav.Link as={Link} to="/login">Login</Nav.Link>
+                    <Nav.Link as={Link} to="/addStock">AddStock</Nav.Link>
                 </Nav>
                 </Navbar.Collapse>
             </Container>
