a03cd02851dcff003d7d23831d9e1d7c0380cf39
sujon-chambugong <sujonchambugong@gmail.com>
2022-04-29 17:37:20 +0600

    login and register page added and also firebase install and firebse hook

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import './App.css';
 import AddStock from './components/AddStock/AddStock';
 import UpdateStock from './components/AddStock/UpdateStock';
 import Login from './components/AuthenLogin/Login';
+import Register from './components/AuthenLogin/Register';
 import About from './components/Pages/About';
 import Blog from './components/Pages/Blog';
 import Home from './components/Pages/Home';
@@ -20,6 +21,7 @@ function App() {
         <Route path='/' element={<Home></Home>}></Route>
         <Route path='/about' element={<About></About>}></Route>
         <Route path='/login' element={<Login></Login>}></Route>
+        <Route path='/register' element={<Register></Register>}></Route>
         <Route path='/blog' element={<Blog></Blog>}></Route>
         <Route path='/addStock' element={<AddStock></AddStock>}></Route>
         <Route path='/inventory' element={<Inventory></Inventory>}></Route>
--- src/components/AuthenLogin/Register.js
+++ src/components/AuthenLogin/Register.js
@@ -5,31 +5,36 @@ import SocialLogin from './SocialLogin';
 
 const Register = () => {
     return (
-        <div className='container'>
+        <div className='container mt-5'>
         <div className='row'>
         <div class="col-md-6 offset-md-3">
-            <h1>Login</h1>
+            <h1>Register</h1>
             <Form>
+                <Form.Group className="mb-3" controlId="formBasicName">
+                    <Form.Label>Your name</Form.Label>
+                    <Form.Control type="text" placeholder="Enter name" />
+                </Form.Group>
+
                 <Form.Group className="mb-3" controlId="formBasicEmail">
                     <Form.Label>Email address</Form.Label>
                     <Form.Control type="email" placeholder="Enter email" />
-                    <Form.Text className="text-muted">
-                    We'll never share your email with anyone else.
-                    </Form.Text>
                 </Form.Group>
 
                 <Form.Group className="mb-3" controlId="formBasicPassword">
                     <Form.Label>Password</Form.Label>
                     <Form.Control type="password" placeholder="Password" />
                 </Form.Group>
-                <Form.Group className="mb-3" controlId="formBasicCheckbox">
-                    <Form.Check type="checkbox" label="Check me out" />
+
+                <Form.Group className="mb-3" controlId="formBasicConfirmPassword">
+                    <Form.Label>Confirm Password</Form.Label>
+                    <Form.Control type="password" placeholder="Confirm Password" />
                 </Form.Group>
+                
                 <Button variant="primary" type="submit">
-                    Login
+                    Register
                 </Button>
             </Form>
-            <p>Already register at Ware Houe? Please <Link to="/login">Register</Link> here</p>
+            <p>Already register at Ware Houe? Please <Link to="/login">Login</Link> here</p>
             <SocialLogin></SocialLogin>
             </div>
         </div>
